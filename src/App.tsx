154dfdd185d3bfc313207fe292e223416f68b44e
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-01 00:54:52 +0200

    Fix naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Footer from './components/Footer';
 import LoginPage from './pages/LoginPage';
 import TasksPage from './pages/TasksPage';
 
 const App: React.FC = () => (
-  <BrowserRouter>
+  <Router>
     <div className='container'>
       <Routes>
-        <Route path='/login' element={LoginPage}/>
-        <Route path='/tasks' element={TasksPage}/>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/tasks' element={<TasksPage />}/>
       </Routes>
+      <Footer/>
     </div>
-    <Footer/>
-  </BrowserRouter>
+  </Router>
 );
 
 export default App;
