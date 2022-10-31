b3c6086b15d6c95b1db3b63be146418697fdc8fa
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-10-31 21:22:45 +0200

    Added footer to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
+import Footer from './components/Footer';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <div className='container'>
       <Switch></Switch>
     </div>
+    <Footer/>
   </BrowserRouter>
 );
 
