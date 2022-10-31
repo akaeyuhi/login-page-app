35ec01696cfa61899fbc86eca99921462f9c8a68
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-01 00:56:39 +0200

    Fixed LoginForm layout

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -4,11 +4,20 @@ export default function LoginForm() {
   return (
     <form className="main__login">
       <h2 className='main__welcome'>Welcome</h2>
-      <input type='text' id="email" name="email" />
-      <input type='text' id="password" name="password" />
-      <input type='checkbox' id="remember" name="remember">
-        <label className='main__label'>Remember me</label>
-      </input>
+      <input type='text' id="email" name="email" placeholder="Email" className="main__input"/>
+      <input
+        type="text"
+        id="password"
+        name="password"
+        placeholder="Password"
+        className="main__input"
+      />
+      <div className='main__checkbox-container control-group'>
+        <label className='control control-checkbox' htmlFor="remember">Remember me
+          <input type="checkbox"/>
+          <div className='control_indicator'/>
+        </label>
+      </div>
       <input type='submit' value='Login' className='main__submit' />
     </form>
   );
