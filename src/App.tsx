c04a95ae04684fa0aed96939a19a838c2e0617d3
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-10-31 21:35:49 +0200

    Added empty pages to switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer';
+import LoginPage from './pages/LoginPage';
+import TasksPage from './pages/TasksPage';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <div className='container'>
-      <Switch></Switch>
+      <Switch>
+        <Route path='/login' component={LoginPage}/>
+        <Route path='/tasks' component={TasksPage}/>
+      </Switch>
     </div>
     <Footer/>
   </BrowserRouter>
