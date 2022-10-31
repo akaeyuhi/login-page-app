b7d8058245bb628755751aa087c9cbe5d44ba458
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-01 01:48:17 +0200

    Tasks page

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Tasks from '../components/Tasks';
 
 export default function TasksPage() {
-  return <main className="main"></main>;
+  return <main className="main">
+    <Tasks />
+  </main>;
 }
