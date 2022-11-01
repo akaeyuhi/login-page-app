9b06b8fea151a871cb9c0faf3a66107203747fb3
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-01 19:37:41 +0200

    Added login check

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import { Routes, Route } from 'react-router';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router';
 import Footer from './components/Footer';
 import LoginPage from './pages/LoginPage';
 import TasksPage from './pages/TasksPage';
+import { useSelector } from 'react-redux';
+import { RootState } from './store';
 
-const App: React.FC = () => (
-  <Router>
-    <div className='container'>
-      <Routes>
-        <Route path='/login' element={<LoginPage />}/>
-        <Route path='/tasks' element={<TasksPage />}/>
-      </Routes>
-      <Footer/>
-    </div>
-  </Router>
-);
+const App: React.FC = () => {
+  const navigate = useNavigate();
+  const { token } = useSelector((state: RootState) => state.auth);
+
+  useEffect(() => {
+    if (token) {
+      navigate('/tasks');
+    } else {
+      navigate('/login');
+    }
+  }, [token]);
+  return (<div className='container'>
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/tasks' element={<TasksPage />} />
+    </Routes>
+    <Footer />
+  </div>);
+};
 
 export default App;
