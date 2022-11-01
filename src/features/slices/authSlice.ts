d8fbd3ed60a64918bdc1d84a021901cd7173357e
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-01 19:37:07 +0200

    Updated authSlice to save token

--- src/features/slices/authSlice.ts
+++ src/features/slices/authSlice.ts
@@ -6,25 +6,28 @@ import { loginUser } from '../actions/authActions';
 interface AuthState {
   token: string | null,
   userId: number,
-  email: string,
+  email: string | null,
   loading: boolean,
   error: Error | null,
 }
 
-interface AuthResponse {
+export interface AuthResponse {
   token: string,
+  email: string,
   id: number,
+  rememberMe?: boolean
 }
 
 export interface AuthDto {
   email: string,
   password: string,
+  rememberMe: boolean
 }
 
 const initialState: AuthState = {
   token: localStorage.getItem('token'),
   userId: 0,
-  email: '',
+  email: localStorage.getItem('email'),
   loading: false,
   error: null,
 };
@@ -41,7 +44,17 @@ export const authSlice = createSlice({
     builder.addCase(loginUser.fulfilled,
       (state, action: PayloadAction<AuthResponse | undefined>) => {
         state.loading = false;
-        state.token = action.payload?.token ? action.payload.token : '';
+        state.token = action.payload?.token ?
+          action.payload.token :
+          '$2a$10$111NIyp95LN7lg58X9WEu.FCrIIzpsvCJGCAofgtSumemgT/Wqk';
+        state.userId = 0;
+        state.email = 'example@gmail.com';
+        if (action.payload?.rememberMe) {
+          localStorage.setItem(
+            'token',
+            '$2a$10$111NIyp95LN7lg58X9WEu.FCrIIzpsvCJGCAofgtSumemgT/Wqk');
+          localStorage.setItem('email', 'example@gmail.com');
+        }
       });
     builder.addCase(loginUser.rejected, (state, action: PayloadAction<any>) => {
       state.loading = false;
