4eece0f625abfd29cdb12021234dfc5919aa4222
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-01 19:37:27 +0200

    Commented actual code to creat stub data

--- src/features/actions/authActions.ts
+++ src/features/actions/authActions.ts
@@ -1,22 +1,27 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { AuthDto } from '../slices/authSlice';
+import { AuthDto, AuthResponse } from '../slices/authSlice';
 
 
 export const loginUser = createAsyncThunk(
   'auth/login',
-  async ({ email, password }: AuthDto, { rejectWithValue }) => {
+  async ({ email, rememberMe }: AuthDto, { rejectWithValue }) => {
     try {
-      const headers = {
-        'Content-Type': 'application/json',
+      // const headers = {
+      //   'Content-Type': 'application/json',
+      // };
+      const data: AuthResponse = {
+        token: '',
+        email/*, password*/, rememberMe, id: 0,
       };
-      const data: AuthDto = {
-        email, password,
-      };
-      await fetch('/login', {
-        method: 'POST',
-        headers,
-        body: JSON.stringify(data),
-      });
+      return data;
+      // return {
+      //   ...await (await fetch('/login', {
+      //     method: 'POST',
+      //     headers,
+      //     body: JSON.stringify(data),
+      //   })).json(),
+      //   rememberMe,
+      // };
     } catch (error: any) {
       if (error.response && error.response.data.message) {
         return rejectWithValue(error.response.data.message);
